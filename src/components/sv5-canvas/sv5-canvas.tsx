bdfe9eeca31ae188ab3dc39a6eb6ca7864448398
rasor <soren@rasor.dk>
2019-03-17 13:41:35 +0100

    loopShapes()

--- src/components/sv5-canvas/sv5-canvas.tsx
+++ src/components/sv5-canvas/sv5-canvas.tsx
@@ -30,8 +30,18 @@ export class Sv5Canvas {
   private getText(): string {
     return format(this.first, this.middle, this.last);
   }
-  private toRect(s: Shape): Rect{
-    return s as Rect;
+  // private toRect(s: Shape): Rect{
+  //   return s as Rect;
+  // }
+  private drawRect(s: Rect) {
+    return <rect x={s.x} y={s.y} width={s.width} height={s.heigth} stroke={s.stroke} fill={s.fill} ></rect>;
+  }
+  private loopShapes(shapes: Shape[]) {
+    return shapes.map((s) => 
+      {
+        return this.drawRect(s as Rect);
+      }
+    );
   }
 
   render() {
@@ -43,7 +53,7 @@ export class Sv5Canvas {
     console.log(this.shapes);
     return <svg x="0px" y="0px" width="300px" height="100px" viewBox="0 0 300 100">
       <rect x="10px" y="5px" width="85px" height="90px" stroke="black" fill="white" ></rect>
-      {this.shapes.map((s) => <rect x={s.x} y={s.y} width={this.toRect(s).width} height={this.toRect(s).heigth} stroke={s.stroke} fill={s.fill} ></rect> )}
+      {this.loopShapes(this.shapes)}
     </svg>;
   }
 }
